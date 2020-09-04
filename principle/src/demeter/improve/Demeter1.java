cdab63746db5b204488e1bbe0dd11525a4cd2ba6
JimsChan <280343091@qq.com>
2020-09-04 15:12:03 +0800

    demeter 补充

---
+++ principle/src/demeter/improve/Demeter1.java
@@ -0,0 +1,92 @@
+package demeter.improve;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author jamesChan
+ * @date 2020/9/4 - 14:45
+ */
+
+//客户端
+public class Demeter1 {
+    public static void main(String[] args) {
+        SchoolManager schoolManager = new SchoolManager();
+        schoolManager.printAllEmployee(new CollegeManager());
+    }
+}
+
+//学校总部员工
+class Employee {
+    private String id;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
+
+//学院员工
+class CollegeEmployee {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
+
+//管理学院员工的管理类
+class CollegeManager {
+    //返回去学院的所有员工
+    public List<CollegeEmployee> getAllEmployee() {
+        List<CollegeEmployee> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {//这里我们增加了10个员工到List
+            CollegeEmployee emp = new CollegeEmployee();
+            emp.setId("学院员工id=" + i);
+            list.add(emp);
+        }
+        return list;
+    }
+
+    //输出学院员工的信息
+    public void printEmployee(){
+        List<CollegeEmployee> list1 = getAllEmployee();
+        System.out.println("--------分公司员工--------");
+        for (CollegeEmployee e : list1) {
+            System.out.println(e.getId());
+        }
+    }
+}
+
+//学校管理类
+class SchoolManager {
+    //返回学校总部的员工
+    public List<Employee> getAllEmployee() {
+        List<Employee> employees = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Employee emp = new Employee();
+            emp.setId("学校总部员工id=" + i);
+            employees.add(emp);
+        }
+        return employees;
+    }
+
+    //该方法完成输出学校
+    public void printAllEmployee(CollegeManager sub) {
+        //获取到学院员工
+        sub.printEmployee();
+        //获取到学校总部员工
+        List<Employee> list2 = this.getAllEmployee();
+        System.out.println("--------学校总部员工--------");
+        for (Employee e : list2) {
+            System.out.println(e.getId());
+        }
+    }
+}
